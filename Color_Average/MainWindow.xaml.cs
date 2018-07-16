2b2dae26527d3c2a720f54c6cef71feb39e2f5a6
Jeremiah Berger <j.berger@plastictechnologies.com>
2018-07-16 16:45:19 -0400

    7_16_2018

--- Color_Average/MainWindow.xaml.cs
+++ Color_Average/MainWindow.xaml.cs
@@ -17,8 +17,10 @@ namespace Color_Average
         public MainWindow()
         {
             InitializeComponent();
-            // this is taking a while, but not if it was Danny
+            
         }
+        
+
 
         private void ImportButton_Click(object sender, RoutedEventArgs e)
         {
@@ -81,6 +83,11 @@ namespace Color_Average
             }
         }
 
+        public bool AllowCursor;
+        public int ImageWidthHalf;
+        public int PixelateIntensity;
+        public Bitmap UserImage;
+
         private void PixelatorButton_Click(object sender, RoutedEventArgs e)
         {
 
@@ -98,59 +105,54 @@ namespace Color_Average
             if (op.ShowDialog() == true)
             {
                 string path = new Uri(op.FileName).LocalPath.ToString();
-                Bitmap UserImage = new Bitmap(path);
+                UserImage = new Bitmap(path);
                 BitmapImage ImgSrc = new BitmapImage(new Uri(op.FileName));
-                Image.Source = ImgSrc;
-                int TotalPixels = (UserImage.Height) * (UserImage.Width);
-                int RectangleWidth = ((400 * ((UserImage.Width) / (UserImage.Height)) * (UserImage.Width)) / UserImage.Height);
+                ToBePixelated.Source = ImgSrc;
+                ImageWidthHalf = (((UserImage.Height) / (600)) * (UserImage.Width)) / 2 ;
+
+                AllowCursor = true;
 
 
 
-                
 
 
-                for (int i = 0; i < UserImage.Width; i++)
-                {
-                    for (int j = 0; j < UserImage.Height; j++)
-                    {
-                        System.Drawing.Color pixel = UserImage.GetPixel(i, j);
-                        byte red = pixel.R;
-                        byte green = pixel.G;
-                        byte blue = pixel.B;
-
-                        TotalRed = TotalRed + red;
-                        TotalGreen = TotalGreen + green;
-                        TotalBlue = TotalBlue + blue;
-                        pixelcount++;
-
-                    }
-                }
-
-                double RAvg = TotalRed / pixelcount;
-                double GAvg = TotalGreen / pixelcount;
-                double BAvg = TotalBlue / pixelcount;
-
-
-                ColorDisplay.Fill = new SolidColorBrush(System.Windows.Media.Color.FromRgb((byte)RAvg, (byte)GAvg, (byte)BAvg));
-                ColorDisplay.Width = RectangleWidth;
-
-
-                RGBText.Text = "rgb(" + (byte)RAvg + ", " + (byte)GAvg + ", " + (byte)BAvg + ")";
-                int RInt = Convert.ToInt16(RAvg);
-                int GInt = Convert.ToInt16(GAvg);
-                int BInt = Convert.ToInt16(BAvg);
-
-                string hex = RInt.ToString("X2") + GInt.ToString("X2") + BInt.ToString("X2");
-                HexText.Text = "#" + hex;
             }
         }
 
+               
+            
+        
+
         private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-
-
-            var pos = e.GetPosition(this.ToPixelImage);
             
+          if (AllowCursor == true)
+         {
+                PixelateIntensity = 20;
+
+
+               var pos = e.GetPosition(this.ToPixelImage);
+              XLabel.Content = pos.X;
+               YLabel.Content = pos.Y;
+
+              
+
+
+
+                System.Drawing.Color PhotoColor = UserImage.GetPixel(Convert.ToInt16(pos.X), Convert.ToInt16(pos.Y));
+                byte red = PhotoColor.R;
+                byte green = PhotoColor.G;
+                byte blue = PhotoColor.B;
+
+
+            }
+
+           else;
+          {
+        }
+
+
+
         }
     }
 }
